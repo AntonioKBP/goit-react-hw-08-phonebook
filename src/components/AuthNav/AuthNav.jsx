f590e8c498e43ce25e1af8ae14859c300820b639
Anton Kovsh <kovsh_anton@ukr.net>
2023-02-26 12:11:42 +0200

    upd

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,49 +1,26 @@
-import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectAuthToken } from 'redux/auth/auth-selectors';
-import { useEffect } from 'react';
-import { selectProfile } from 'redux/profile/profile.selectors';
-import { getProfileThunk } from 'redux/profile/profile.thunk';
-import { authLogoutThunk } from 'redux/auth/auth.thunk';
+import { selectAuthLoggedIn, selectAuthName } from 'redux/auth/auth-selectors';
+import { NavLink } from 'react-router-dom';
 
-export const AuthNav = () => {
-  const token = useSelector(selectAuthToken);
+export const AppBar = () => {
+  const isLoggedIn = useSelector(selectAuthLoggedIn);
+  const name = useSelector(selectAuthName);
   const dispatch = useDispatch();
-  const profile = useSelector(selectProfile);
-  // const navigate = useNavigate();
-
-  useEffect(() => {
-    if (token) {
-      dispatch(getProfileThunk());
-    }
-  }, [token, dispatch]);
-
-  // const handleLogOut = () => {
-  //   dispatch(authLogoutThunk());
-  //   navigate('/', { replace: true });
-  // };
-
   return (
-    <div>
-      <div>
-        {!token && <p>Please Login</p>}
-        {token && profile && (
-          <>
-            <h2>Welcome Back {profile.name}</h2>
-            <button onClick={() => dispatch(authLogoutThunk())}>Log Out</button>
-            <NavLink to={'/contacts'}>Contacts</NavLink>
-          </>
-        )}
-        {token ? (
-          <></>
-        ) : (
-          <>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'sign-up'}>Sign Up</NavLink>
-            <NavLink to={'login'}>Login</NavLink>
-          </>
-        )}
+    <header>
+      <div className="wrapper">
+        <nav>
+          {isLoggedIn ? (
+            <button onClick={() => dispatch()}>LogOut</button>
+          ) : (
+            <>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/signin">Sign In</NavLink>
+            </>
+          )}
+        </nav>
+        {isLoggedIn && <p>Hello {name}</p>}
       </div>
-    </div>
+    </header>
   );
 };
