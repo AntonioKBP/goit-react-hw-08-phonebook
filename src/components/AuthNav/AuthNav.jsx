6eb54bd69a96c164eaf07d36b9add98d6c435057
Anton Kovsh <kovsh_anton@ukr.net>
2023-02-26 17:39:10 +0200

    check

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,26 +1,50 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectAuthLoggedIn, selectAuthName } from 'redux/auth/auth-selectors';
+import { UserMenu } from 'components/UserMenu/UserMenu';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAuthToken } from 'redux/auth/auth-selectors';
+import { useEffect } from 'react';
+import { selectProfile } from 'redux/profile/profile.selectors';
+import { getProfileThunk } from 'redux/profile/profile.thunk';
+import { authLogoutThunk } from 'redux/auth/auth.thunk';
 
-export const AppBar = () => {
-  const isLoggedIn = useSelector(selectAuthLoggedIn);
-  const name = useSelector(selectAuthName);
+export const AuthNav = () => {
+  const token = useSelector(selectAuthToken);
   const dispatch = useDispatch();
+  const profile = useSelector(selectProfile);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getProfileThunk());
+    }
+  }, [token, dispatch]);
+
+  // const handleLogOut = () => {
+  //   dispatch(authLogoutThunk());
+  //   navigate('/', { replace: true });
+  // };
+
   return (
-    <header>
-      <div className="wrapper">
-        <nav>
-          {isLoggedIn ? (
-            <button onClick={() => dispatch()}>LogOut</button>
-          ) : (
-            <>
-              <NavLink to="/login">Login</NavLink>
-              <NavLink to="/signin">Sign In</NavLink>
-            </>
-          )}
-        </nav>
-        {isLoggedIn && <p>Hello {name}</p>}
+    <div>
+      <div>
+        {!token && <p>Please Login</p>}
+        {token && profile && (
+          <>
+            <h2>Welcome Back {profile.name}</h2>
+            <button onClick={() => dispatch(authLogoutThunk())}>Log Out</button>
+            <NavLink to={'/contacts'}>Contacts</NavLink>
+          </>
+        )}
+        {token ? (
+          <></>
+        ) : (
+          <>
+            <NavLink to={'/'}>Home</NavLink>
+            <NavLink to={'sign-up'}>Sign Up</NavLink>
+            <NavLink to={'login'}>Login</NavLink>
+          </>
+        )}
       </div>
-    </header>
+    </div>
   );
 };
